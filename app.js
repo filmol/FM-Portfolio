be2e75586cde0df60e6f59d51073a5153fc8a42d
Filip-Moltzer <filip.moltzer@gmail.com>
2019-05-06 20:32:42 +0200

    start of redesign from grid to flexbox fot the projects site

---
+++ app.js
@@ -0,0 +1,31 @@
+const getClass = function (h) {
+ return document.getElementsByClassName(h)[0];
+}
+const getID = function (h) {
+ return document.getElementById(h);
+}
+const removeActive = function() {
+    getClass("active").setAttribute("class", "no-active");
+    getClass("focus").setAttribute("class", "no-focus");
+  }
+const addActive = function(x) {
+  getID("slide"+x).setAttribute("class", "active");
+  getID("dot"+x).setAttribute("class", "focus");
+}
+
+getID("dot1").onclick = function() {
+   removeActive();
+   addActive("1");
+ }
+
+getID("dot2").onclick = function() {
+   removeActive();
+   addActive("2");
+ }
+
+getID("dot3").onclick = function() {
+   removeActive();
+   addActive("3");
+ }
+
+ 
