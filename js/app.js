7642e58474a1bc9c4e3e2607364272a24f40b5e3
Filip-Moltzer <filip.moltzer@gmail.com>
2019-05-17 01:16:05 +0200

    Memory game linked to projects site

--- js/app.js
+++ js/app.js
@@ -35,7 +35,23 @@ getID("dot3").onclick = function() {
  }
 }
 
-// if it's not homepage
+// Check if it's Memory Game page
+else if (body.classList.contains("memory-game")) {
+
+}
+
+else if (body.classList.contains("projects")) {
+  const memoryImg = document.getElementById("memoryImage");
+
+  memoryImg.style.cursor = "pointer"
+
+  memoryImg.onclick = function() {
+    window.open("/Users/filipmoltzer/Desktop/UDACITY 2.0/P2 - Memory Game/index.html");
+  };
+}
+
+
+// Check if it's Skills page
 else {
 const githubLogo = document.getElementById("github-logo");
 // Github pointer when hovering.
