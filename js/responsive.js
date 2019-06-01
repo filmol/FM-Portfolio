5aec8e1b2b71d3ec0d6d882ccf62abec7886ca0a
Filip-Moltzer <filip.moltzer@gmail.com>
2019-06-01 14:34:36 +0200

    Styled crisp and clear buttons

--- js/responsive.js
+++ js/responsive.js
@@ -1,6 +1,7 @@
 let menu = document.getElementsByClassName("dropbtn")[0]
 let counter = 0;
 
+// Checks if menu is open
 let menuOpen = function () {
   if (counter == 0) {
     document.getElementsByClassName("dropdown-content")[0].style.display = "block";
@@ -13,3 +14,14 @@ let menuOpen = function () {
 }
 
 menu.addEventListener("click", menuOpen);
+
+// Closes Dropdown menu if main is clicked
+document.getElementsByClassName("main")[0].addEventListener("click", function() {
+  if (counter == 1) {
+    document.getElementsByClassName("dropdown-content")[0].style.display = "none";
+    counter=0;
+  }
+  else {
+    0;
+  }
+});
